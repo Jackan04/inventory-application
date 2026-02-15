74e0e6eb36bac8a082ffb3356927ff99d6c68ba1
jackan04 <jacob.asker@icloud.com>
2026-02-15 12:44:31 +0100

    - Minor layout change for views. - Corrected href for category items in index.ejs to direct users to their respective category pages.

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,10 +3,10 @@ import categoriesController from "../controllers/categoriesController.js";
 
 const categoriesRoutes = Router();
 
-categoriesRoutes.get("/:id", categoriesController.category_show);
-
 categoriesRoutes.get("/create", categoriesController.category_create_get);
 
 categoriesRoutes.post("/create", categoriesController.category_create_post);
+
+categoriesRoutes.get("/:id", categoriesController.category_show);
 
 export default categoriesRoutes;
